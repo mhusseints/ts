e53e39e040218325bfa3f0931ea106f6605dc3e9
mhusseints <m.hussein.ts@gmail.com>
2024-07-14 14:48:08 +0300

    modify name of table as param

--- select sessions id by tablename.sql
+++ select sessions id by tablename.sql
@@ -3,5 +3,5 @@ SELECT sid, serial#, status, username FROM v$session;
 
 select * from V$ACCESS VA,v$session VS
 where VS.SID=VA.SID
-AND OBJECT LIKE '%PAT_PROC%'
+AND OBJECT LIKE '%'||:tableName||'%'
 AND STATUS = 'ACTIVE';
